183860e2733d9569f60dc84b5df55e848f56bca1
Max Waldner <maxwaldner@hotmail.com>
2024-11-24 17:36:50 -0600

    Added Vehicle date structure, file organization and dynamic vehicle info

---
+++ Functions.js
@@ -0,0 +1,15 @@
+export function moveToInsurance() {
+    var image = document.getElementById("progress_car");
+    image.classList = "move_insurance";
+}
+
+export function moveToAddons() {
+    var image = document.getElementById("progress_car");
+    image.classList = "move_addons";
+}
+
+export function moveToVehicles() {
+    var image = document.getElementById("progress_car");
+    image.classList = "move_vehicles";
+}
+
